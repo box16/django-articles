6c529e810b34e321e07cc1a12a9c2aa2634333a2
box16 <inoinobox16@gmail.com>
2021-01-20 11:12:23 +0900

    articlesの削除

--- articles/management/commands/train_article.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from articles.extensions.d2v import D2V
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        d2v = D2V()
-        d2v.training()
